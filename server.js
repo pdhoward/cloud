6c4446496f4b41785b664e440f9f8a54b2272bf1
pdhoward <patrick.howard@hotmail.com>
2019-01-18 10:22:28 -0500

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const path = require('path')
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router(path.join(__dirname, './json/db.json'))
 const middlewares = jsonServer.defaults()
 
 const Port = 4000
@@ -28,13 +28,16 @@ server.get('/echo', (req, res) => {
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
-server.use((req, res, next) => {
+server.use(async (req, res, next) => {
     if (req.method === 'POST') {
         req.body.createdAt = Date.now()
-        res.json(data) 
+        let data = await require('./data')()
+        console.log(data)
+        //res.json(data) 
+        next(data)
     }
     // Continue to JSON Server router
-    next()
+   // next()
 })
 
 server.use(router)
